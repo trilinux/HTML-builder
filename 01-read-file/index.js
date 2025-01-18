aae9f5d7a43af3a8dc07a85a45a29d6777718af2
Dima @trilinux <linuxoutpost@gmail.com>
2025-01-18 23:59:27 +0100

    fix: 01-read-file solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
 const { stdout: output } = require('process');
 
 const filePath = path.join(__dirname, 'text.txt');
-readline.createInterface({ input: fs.createReadStream(filePath), output });
+const readStream = fs.createReadStream(filePath, 'utf8');
+readStream.on('data', (chunk) => {
+  output.write(chunk);
+});
